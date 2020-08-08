726a930d952cd9021887e96362246e83c2f50226
Francine Guimarães <francinesguimaraes@outlook.com>
2020-08-08 00:34:52 -0300

    Basic data processing and template engine

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-const proffys = [
+const proffys = 
+[
     {
         name: "Francine Guimarães",
         avatar: "https://scontent.fpoa39-1.fna.fbcdn.net/v/t1.0-9/95092407_1577915349041529_5878084020738195456_o.jpg?_nc_cat=110&_nc_sid=454319&_nc_ohc=ojHeIv29QCEAX-aHkoG&_nc_ht=scontent.fpoa39-1.fna&oh=563b101eac1ca263128acd1c68d39a3a&oe=5F519C2C",
         whatsapp: "0197907265",
-        bio: "Apaixonada por números.<br/>Amo dividir minha paixão pelos números com o mundo!",
+        bio: "Apaixonada por números. Amo dividir minha paixão pelos números com o mundo!",
         subject: "Matemática",
-        cost: "10,00",
+        cost: "10",
         weekday: [1,3,5],
         time_from: [1450],
         time_to: [1800]
@@ -17,27 +18,76 @@ const proffys = [
         whatsapp: "0123654747",
         bio: "Professor de português ha 10 anos.<br/>Acredito que a educação é um dos pilares essencias para tornar o mundo um lugar melhor.",
         subject: "Português",
-        cost: "15,00",
+        cost: "15",
         weekday: [2,4,6],
         time_from: [1250],
         time_to: [1600]
     }
 ]
 
+const subjects = 
+[
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Música",
+    "Potuguês",
+    "Química"
+]
+
+const weekdays =
+[
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+// functionalities
+
+function getSubject(subjectNumber) { // it takes subject number and returns subject string.
+    const position = +subjectNumber - 1; // + means that the variabe is a integer type.
+    return subjects[position];
+}
+
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html");
+    return res.render("index.html");
 }
 
 function studyPage(req, res) {
-    return res.sendFile(__dirname + "/views/study.html");
+    const filters = req.query;
+    return res.render("study.html", {proffys, filters, subjects, weekdays});
 }
 
 function teachPage(req, res) {
-    return res.sendFile(__dirname + "/views/teach.html");
+    const data = req.query; // catch the data awnser from front-end.
+
+    const isNotEmpty = Object.keys(data).length > 0; // it transforms data in an array, then, we can check if data is empty or not.
+
+    if (isNotEmpty){
+        data.subject = getSubject(data.subject);
+        proffys.push(data); // adding data to the proffys' list.
+        return res.redirect("/study");
+    }
+    return res.render("teach.html", {subjects, weekdays});
 }
 
+// server:
 const express = require("express");
 const server = express();
+
+const nunjucks = require("nunjucks"); // config nunjucks (template engine).
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
 
 server
 .use(express.static("public")) // server configuration to get the public folder + routes.
